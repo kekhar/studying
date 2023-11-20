1ae6fa840d54059bd4e0832098e1b746c8ef49d8
Kirill <m_kirill72@mail.ru>
2023-11-20 19:50:17 +0300

    start js Код ревью - 1

---
+++ 4-navigator/app.js
@@ -0,0 +1,12 @@
+// Данные
+const addressLat = 10;
+const addressLong = 20;
+const positionLat = 15;
+const positionLong = 25;
+
+// Счет
+const lat2 = 15 - 10;
+const lon2 = 25 - 20;
+
+// Результат
+console.log(lat2 + lon2);
