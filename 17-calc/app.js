d9f9174ac35285c4b11dbd52f11fcca29c2c6e9b
Kirill <m_kirill72@mail.ru>
2024-01-22 22:07:47 +0300

    fix#4

--- 17-calc/app.js
+++ 17-calc/app.js
@@ -1,46 +1,41 @@
-document.getElementById('operationButtons').addEventListener('click', function (event) {
-  if (event.target.tagName === 'BUTTON') {
-    let operator = event.target.getAttribute('data-operation');
-    operation(operator);
-  }
-});
+document
+  .getElementById("operationButtons")
+  .addEventListener("click", function (event) {
+    if (event.target.tagName === "BUTTON") {
+      let operator = event.target.getAttribute("data-operation");
+      operation(operator);
+    }
+  });
 
 function operation(operator) {
-  let num1 = parseFloat(document.getElementById('num1').value);
-  let num2 = parseFloat(document.getElementById('num2').value);
-  let resultElement = document.getElementById('result');
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  let resultElement = document.getElementById("result");
 
   if (isNaN(num1) || isNaN(num2)) {
-    resultElement.textContent = 'Введите корректные числа';
+    resultElement.textContent = "Введите корректные числа";
     return;
   }
 
   let result;
-  switch (operator) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-      if (num2 !== 0) {
-        result = num1 / num2;
-      } else {
-        resultElement.textContent = 'На ноль делить нельзя';
-        return;
-      }
-      break;
-    default:
-      resultElement.textContent = 'Неверный оператор';
-      return;
+  const mathOperations = {
+    "+": (val1, val2) => val1 + val2,
+    "-": (val1, val2) => val1 - val2,
+    "*": (val1, val2) => val1 * val2,
+    "/": (val1, val2) => (val2 !== 0 ? val1 / val2 : undefined),
+  };
+
+  if (mathOperations.hasOwnProperty(operator)) {
+    let result = mathOperations[operator](num1, num2);
+    if (result !== undefined) {
+      resultElement.textContent = "Результат: " + result;
+    } else {
+      resultElement.textContent = "На ноль делить нельзя";
+    }
+  } else {
+    resultElement.textContent = "Неверный оператор";
   }
 
-  resultElement.textContent = 'Результат: ' + result;
-
-  document.getElementById('num1').value = '';
-  document.getElementById('num2').value = '';
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
