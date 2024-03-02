0422eee43b7cbd5506439302b65ccdc9a50de0e6
Kirill <m_kirill72@mail.ru>
2024-03-02 04:06:34 +0300

    js-adv Код-ревью 5

---
+++ 15-build/user.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import Task from './task.js';
+
+export default class User {
+  constructor() {
+    this.task = new Task('Задача запущена');
+  }
+
+  do() {
+    this.task.run();
+  }
+}
